2219a0ae90605840bb72c80fda7252ffe6ffce39
Douglas Cooper <doug@Douglass-MBP.attlocal.net>
2023-05-23 18:24:08 -0700

    added styled components for easier prototyping and parsed out summary and action items from the response

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,19 @@
 'use client';
 import React, {useState} from "react";
 import {commentThread, Comment} from "@/mockData";
+import styled from "styled-components";
+
+const SummaryContainer = styled.div`
+  padding: 1rem;
+  border: 1px solid;
+`
+
+const ActionItemsContainer = styled.div`
+  padding: 1rem;
+  border: 1px solid;
+`
+
+
 
 let extractedComments: any = []
 function extractCommentMetaData() {
@@ -11,12 +24,13 @@ function extractCommentMetaData() {
 
 extractCommentMetaData()
 
-const context = "create a bug report"
+const context = "You are a product manager wanting a status update on the state of the product feature for a login page."
 
-const prompt = `Given the ${context}, and the comments ${extractedComments}, generate two sections. One as as summary and the other to identify the action items based on the questions or @ mentions. Don't show unspecified action items`
+const prompt = `Given the context: ${context}, and the comments ${extractedComments}, generate two sections. One as a summary and the other to identify the action items based on the questions or @ mentions. Don't show unspecified action items.`
 
 function Home() {
-  const [answer, setAnswer] = useState(undefined)
+  const [summary, setSummary] = useState<string>('')
+  const [actionItems, setActionItems] = useState<string[]>([])
   const [loading, setLoading] = useState(false)
   async function generateSummary() {
     setLoading(true)
@@ -34,30 +48,77 @@ function Home() {
       throw new Error(response.statusText)
     }
     const summary = await response.json()
-    setAnswer(summary.choices[0].text)
+    await extractSummaryAndActionItems(summary.choices[0].text)
     setLoading(false)
   }
+
+  async function extractSummaryAndActionItems(text: string | undefined) {
+    // Initialize variables
+    let parsedSummary: string = ''
+    let parsedActionItems: string[] = []
+
+    if (text) {
+      // Split the text into lines
+      const lines = text.split('\n')
+
+      // Extract the summary
+      const summaryIndex = lines.indexOf('Summary:') + 2
+      parsedSummary = lines[summaryIndex]
+
+      // Extract the action items
+      const actionItemsIndex = lines.indexOf('Action Items:')
+      for (let i = actionItemsIndex + 1; i < lines.length; i++) {
+        const line = lines[i].trim();
+        if (line.startsWith('-')) {
+          parsedActionItems.push(line.substring(2))
+        }
+      }
+      console.log(parsedSummary)
+      console.log(parsedActionItems)
+      setSummary(parsedSummary)
+      setActionItems(parsedActionItems)
+    }
+
+  }
   return (
-    <main>
-      {commentThread.map((comment: Comment) => {
-        return (
-          <div key={comment.id}>
-            {comment.service}
-            <div>{comment.comment}</div>
-            <div>{comment.author}</div>
-            <div>{comment.created_at}</div>
-            <hr/>
-          </div>
-        )
-      })}
+    <main style={{display: "grid", gridGap: "1rem"}}>
+      <h3>Comment Thread</h3>
+      <div style={{padding: "1rem", border: "1px solid", maxHeight: "15rem", overflowY: "scroll"}}>
+        {commentThread.map((comment: Comment) => {
+          return (
+            <div key={comment.id}>
+              {comment.service}
+              <div>{comment.comment}</div>
+              <div>{comment.author}</div>
+              <div>{comment.created_at}</div>
+              <hr/>
+            </div>
+          )
+        })}
+      </div>
       <button
         disabled={loading}
         type="button"
-        onClick={() => generateSummary()}
-        className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 inline-flex items-center">
+        style={{width: "fit-content"}}
+        onClick={() => generateSummary()}>
         Create Summary
         </button>
-      <div>{answer}</div>
+      <div style={{display: "grid", gridGap: "1rem"}}>
+        <h3>Summary</h3>
+        <SummaryContainer>{summary}</SummaryContainer>
+        <h3>Action Items</h3>
+        <ActionItemsContainer>
+          {actionItems.map((actionItem: string, index) => {
+            return (
+              <>
+                <input id={index.toString()} type="checkbox" key={actionItem} value={actionItem} />
+                <label htmlFor={index.toString()}>{actionItem}</label>
+                <br/>
+              </>
+            )
+          })}
+        </ActionItemsContainer>
+      </div>
     </main>
   )
 }
